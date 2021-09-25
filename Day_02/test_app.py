c54439e9d39bc5d8965a1990ce07ef998bc037cf
Adel NOUAR <adel.nouar@protonmail.com>
2021-09-25 12:43:42 +0000

    Adding pytest-cov and CliRunner to test click

--- Day_02/test_app.py
+++ Day_02/test_app.py
@@ -1,5 +1,13 @@
-from app import change
+from app import change, make_change
+from click.testing import CliRunner
 
 
 def test_change():
     assert [{5: "quarters"}, {1: "nickels"}, {4: "pennies"}] == change(1.34)
+
+
+def test_app():
+    runner = CliRunner()
+    result = runner.invoke(make_change, ["--amount", "1.34"])
+    assert result.exit_code == 0
+    assert "quarters: 5" in result.output
